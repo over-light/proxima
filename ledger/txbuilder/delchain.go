db1f7c42b57878688a1e261a9579d8836a79c349
lunfardo314 <evaldas.drasutis@iota.org>
2025-01-21 14:30:27 +0200

    proxi node delchain modified

---
+++ ledger/txbuilder/delchain.go
@@ -0,0 +1,70 @@
+package txbuilder
+
+import (
+	"crypto/ed25519"
+
+	"github.com/lunfardo314/proxima/ledger"
+	"github.com/lunfardo314/proxima/util"
+)
+
+type DeleteChainParams struct {
+	ChainIn                       *ledger.OutputWithChainID
+	PrivateKey                    ed25519.PrivateKey
+	TagAlongSeqID                 ledger.ChainID
+	TagAlongFee                   uint64 // 0 means no fee output will be produced
+	EnforceNoDelegationTransition bool
+}
+
+func MakeDeleteChainTransaction(par DeleteChainParams) ([]byte, ledger.TransactionID, error) {
+	chainID, _, _ := par.ChainIn.ExtractChainID()
+	// adjust timestamp
+	ts := ledger.MaximumTime(ledger.TimeNow(), par.ChainIn.Timestamp().AddTicks(int(ledger.L().ID.TransactionPace)))
+	if ts.IsSlotBoundary() {
+		ts.AddTicks(1)
+	}
+	if par.ChainIn.Output.Lock().Name() == ledger.DelegationLockName && par.EnforceNoDelegationTransition {
+		ts1 := ledger.NextClosedDelegationTimestamp(chainID, ts)
+		if ts1.Slot() != ts.Slot() {
+			ts = ledger.NewLedgerTime(ts1.Slot(), 1)
+		}
+	}
+
+	_, predecessorConstraintIndex := par.ChainIn.Output.ChainConstraint()
+	txb := New()
+
+	consumedIndex, err := txb.ConsumeOutput(par.ChainIn.Output, par.ChainIn.ID)
+	util.AssertNoError(err)
+
+	feeAmount := par.TagAlongFee
+
+	outNonChain := ledger.NewOutput(func(o *ledger.Output) {
+		o.WithAmount(par.ChainIn.Output.Amount() - feeAmount).
+			WithLock(ledger.AddressED25519FromPrivateKey(par.PrivateKey))
+	})
+	_, err = txb.ProduceOutput(outNonChain)
+	util.AssertNoError(err)
+
+	if feeAmount > 0 {
+		tagAlongFeeOut := ledger.NewOutput(func(o *ledger.Output) {
+			o.WithAmount(feeAmount).
+				WithLock(ledger.ChainLockFromChainID(par.TagAlongSeqID))
+		})
+		if _, err = txb.ProduceOutput(tagAlongFeeOut); err != nil {
+			return nil, ledger.TransactionID{}, err
+		}
+	}
+
+	txb.PutUnlockParams(consumedIndex, predecessorConstraintIndex, []byte{0xff, 0xff, 0xff})
+	txb.PutSignatureUnlock(consumedIndex)
+
+	// finalize the transaction
+	txb.TransactionData.Timestamp = ts
+	txb.TransactionData.InputCommitment = txb.InputCommitment()
+	txb.SignED25519(par.PrivateKey)
+
+	txBytes, txid, _, err := txb.BytesWithValidation()
+	if err != nil {
+		return nil, ledger.TransactionID{}, err
+	}
+	return txBytes, txid, nil
+}
--- proxi/node_cmd/delchain.go
+++ proxi/node_cmd/delchain.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/lunfardo314/proxima/api/client"
 	"github.com/lunfardo314/proxima/ledger"
+	"github.com/lunfardo314/proxima/ledger/txbuilder"
 	"github.com/lunfardo314/proxima/proxi/glb"
 	"github.com/lunfardo314/proxima/util"
 	"github.com/spf13/cobra"
@@ -23,54 +23,70 @@ func initDeleteChainCmd() *cobra.Command {
 	return deleteChainCmd
 }
 
-func deleteChain(chainID ledger.ChainID) (ledger.TransactionID, string, error) {
-	walletData := glb.GetWalletData()
+func runDeleteChainCmd(_ *cobra.Command, args []string) {
+	//cmd.DebugFlags()
+	glb.InitLedgerFromNode()
 
+	chainID, err := ledger.ChainIDFromHexString(args[0])
+	glb.AssertNoError(err)
+
+	walletData := glb.GetWalletData()
 	target := glb.MustGetTarget()
 
 	var tagAlongSeqID ledger.ChainID
 	feeAmount := glb.GetTagAlongFee()
 	glb.Assertf(feeAmount > 0, "tag-along fee is configured 0. Fee-less option not supported yet")
+	clnt := glb.GetClient()
+
 	if feeAmount > 0 {
 		pTagAlongSeqID := glb.GetTagAlongSequencerID()
 		glb.Assertf(pTagAlongSeqID != nil, "tag-along sequencer not specified")
 		tagAlongSeqID = *pTagAlongSeqID
 
-		md, err := glb.GetClient().GetMilestoneData(tagAlongSeqID)
+		md, err := clnt.GetMilestoneData(tagAlongSeqID)
 		glb.AssertNoError(err)
 
 		if md != nil && md.MinimumFee > feeAmount {
 			feeAmount = md.MinimumFee
 		}
 	}
-	glb.Infof("Deleting chain:")
+	chainIN, _, _, err := clnt.GetChainOutput(chainID)
+	glb.AssertNoError(err)
+
+	glb.Infof("deleting chain:")
 	glb.Infof("   chain id: %s", chainID.String())
+	glb.Infof("   chain output: %s", chainIN.ID.String())
+	glb.Infof("   chain controller: %s", target)
 	glb.Infof("   tag-along fee %s to the sequencer %s", util.Th(feeAmount), tagAlongSeqID.String())
 	glb.Infof("   source account: %s", walletData.Account.String())
-	glb.Infof("   chain controller: %s", target)
 
 	if !glb.YesNoPrompt("proceed?:", true, glb.BypassYesNoPrompt()) {
 		glb.Infof("exit")
 		os.Exit(0)
 	}
 
-	return glb.GetClient().DeleteChain(client.DeleteChainParams{
-		WalletPrivateKey: walletData.PrivateKey,
-		TagAlongSeqID:    tagAlongSeqID,
-		TagAlongFee:      feeAmount,
-		ChainID:          chainID,
-	})
-}
+	var txBytes []byte
+	var txid ledger.TransactionID
 
-func runDeleteChainCmd(_ *cobra.Command, args []string) {
-	//cmd.DebugFlags()
-	glb.InitLedgerFromNode()
+	for {
+		txBytes, txid, err = txbuilder.MakeDeleteChainTransaction(txbuilder.DeleteChainParams{
+			ChainIn:                       chainIN,
+			PrivateKey:                    walletData.PrivateKey,
+			TagAlongSeqID:                 chainID,
+			TagAlongFee:                   feeAmount,
+			EnforceNoDelegationTransition: true,
+		})
+		glb.AssertNoError(err)
 
-	chainId, err := ledger.ChainIDFromHexString(args[0])
-	glb.AssertNoError(err)
-
-	txid, failedTx, err := deleteChain(chainId)
-	glb.Assertf(err == nil, "failed to delete chain %s: %v\n----------------- failed tx:\n%s", chainId.String(), err, failedTx)
-
-	glb.ReportTxInclusion(txid, time.Second)
+		leftUntilDelegationClosedSlot := int(ledger.TimeNow().Slot() - txid.Slot())
+		if leftUntilDelegationClosedSlot <= 1 {
+			glb.Infof("submitting transaction %s", txid.String())
+			err = clnt.SubmitTransaction(txBytes)
+			glb.AssertNoError(err)
+			glb.ReportTxInclusion(txid, 2*time.Second)
+			return
+		}
+		glb.Infof("waiting for the slot which is closed for delegation: ~%v..", time.Duration(leftUntilDelegationClosedSlot)*ledger.L().ID.SlotDuration())
+		time.Sleep(ledger.L().ID.SlotDuration())
+	}
 }
