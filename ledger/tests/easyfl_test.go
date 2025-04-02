61e18f4e3889726c786924fa326caa335463672a
lunfardo314 <evaldas.drasutis@iota.org>
2025-04-02 09:49:17 +0300

    easyfl demo test

---
+++ ledger/tests/easyfl_test.go
@@ -0,0 +1,38 @@
+package tests
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/lunfardo314/proxima/ledger"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEasyFL(t *testing.T) {
+	t.Run("compile literal 1", func(t *testing.T) {
+		const source = "12"
+
+		_, nargs, bytecode, err := ledger.L().CompileExpression(source)
+		require.NoError(t, err)
+		t.Logf("src: '%s', args: %d, bytecode: %s", source, nargs, hex.EncodeToString(bytecode))
+	})
+	t.Run("compile literal 2", func(t *testing.T) {
+		const source = "u64/1337"
+
+		_, nargs, bytecode, err := ledger.L().CompileExpression(source)
+		require.NoError(t, err)
+		t.Logf("src: '%s', args: %d, bytecode: %s", source, nargs, hex.EncodeToString(bytecode))
+	})
+	t.Run("compile literal 3", func(t *testing.T) {
+		const source = "concat(u16/15, 255)"
+
+		_, nargs, bytecode, err := ledger.L().CompileExpression(source)
+		require.NoError(t, err)
+
+		res, err := ledger.L().EvalFromBytecode(nil, bytecode)
+		require.NoError(t, err)
+
+		t.Logf("src: '%s', args: %d, bytecode: %s, eval: %s",
+			source, nargs, hex.EncodeToString(bytecode), hex.EncodeToString(res))
+	})
+}
