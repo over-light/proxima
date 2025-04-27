da50afedbeeb14138335136543774bc1cd9234b2
lunfardo314 <evaldas.drasutis@iota.org>
2025-04-27 21:22:11 +0300

    proxi db txstore idlist

--- proxi/db_cmd/txstore/idlist.go
+++ proxi/db_cmd/txstore/idlist.go
@@ -15,7 +15,7 @@ func initIDListCmd() *cobra.Command {
 	idlistCmd := &cobra.Command{
 		Use:   "idlist <slot>",
 		Short: "lists IDs of transactions in slot from the raw txstore",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.MaximumNArgs(1),
 		Run:   runIdListCmd,
 	}
 	idlistCmd.PersistentFlags().BoolVarP(&listAll, "all", "a", false, "list all keys in txstore")
@@ -26,18 +26,22 @@ func runIdListCmd(_ *cobra.Command, args []string) {
 	db := glb.InitDBRaw(global.TxStoreDBName)
 	defer db.Close()
 
-	sint, err := strconv.Atoi(args[0])
-	glb.AssertNoError(err)
-	glb.Assertf(sint <= base.MaxSlot, "wrong slot number")
-	slot := base.Slot(sint)
-
 	var txid base.TransactionID
 	count := 0
 
 	var prefix []byte
+	var slot base.Slot
+	var err error
+
 	if !listAll {
+		glb.Assertf(len(args) == 1, "wrong number of arguments")
+		sint, err := strconv.Atoi(args[0])
+		glb.AssertNoError(err)
+		glb.Assertf(sint <= base.MaxSlot, "wrong slot number")
+		slot = base.Slot(sint)
 		prefix = slot.Bytes()
 	}
+
 	db.Iterator(prefix).IterateKeys(func(k []byte) bool {
 		txid, err = base.TransactionIDFromBytes(k)
 		glb.AssertNoError(err)
