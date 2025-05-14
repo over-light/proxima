0a1edcda34d8ac9e35f9ed700aba1731b880fa7e
lunfardo314 <evaldas.drasutis@iota.org>
2025-05-14 10:13:14 +0300

    wip

--- core/core_modules/branches/branches.go
+++ core/core_modules/branches/branches.go
@@ -86,8 +86,8 @@ func (b *Branches) getNoLock(branchID base.TransactionID) (*multistate.BranchDat
 	if bd, ok := b.m[branchID]; ok {
 		if branchID.Slot() > 0 {
 			// branch record is in the cache. Ledger coverage (calculated) must always be bigger than coverage delta
-			b.Assertf(bd.LedgerCoverage > bd.CoverageDelta, "bd.LedgerCoverage(%s) > bd.CoverageDeltaRaw(%s)",
-				util.Th(bd.LedgerCoverage), util.Th(bd.CoverageDelta))
+			b.Assertf(bd.LedgerCoverage > bd.CoverageDelta, "bd.LedgerCoverage(%s) > bd.CoverageDeltaRaw(%s) for %s",
+				util.Th(bd.LedgerCoverage), util.Th(bd.CoverageDelta), branchID.StringShort)
 		}
 		bd.LastActive = time.Now()
 		return bd, true
